b0c451ef434ad0e07a466c8824ffdd9c597b0cd8
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2024-12-02 16:50:53 +1100

    feat: homepage started

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Background () {
     return (
-        <div className="background"></div>
+        <div className="background" src="../assets/homepage.png">
+            
+        </div>
     )
 }
 
