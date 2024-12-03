edcd60c0de96247dd10406db73dbf3412039e486
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2024-12-04 07:35:34 +1100

    feat: User dashboard demo with mui material

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+// import DashboardLayoutBasic from './pages/UserDashboardDemo';
+
+
 function App() {
 
 
   return (
-    <BrowserRouter>
+    
+    <BrowserRouter> 
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/signup" element={<Signup />}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
+    
+    
   )
 }
 
