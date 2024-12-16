b53b77ba1049e8011f59945a7bf1040f276ce310
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2024-12-16 13:16:04 +1100

    feat: fixed pages' responsiveness

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
 import '../styles/userdashboard.css';
 import DashImg from '../assets/dashboard.jpg' ; 
 import Footer from "../components/Footer";
